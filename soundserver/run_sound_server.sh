a9d666425f4941e8a2900b8bc6734bf773f8625d
Lars Dehler <larsdehler95@yahoo.de>
2020-09-04 15:12:27 +0000

    update soundserver to serve static soundindex.json file

--- soundserver/run_sound_server.sh
+++ soundserver/run_sound_server.sh
@@ -1,4 +1,3 @@
 docker stop sb-nginx &> /dev/null
 docker rm sb-nginx &> /dev/null
-docker build -t sb-nginx . 
-docker run -v $(pwd)/sounds:/sounds -p 8080:80 --name sb-nginx sb-nginx
+docker run $1 -v $(pwd)/conf:/etc/nginx/conf.d -v $(pwd)/data:/data -p 8080:80 --name sb-nginx nginx:latest
